8d638af90806e4b0a0cf37d789f8c8e432e6d431
Ryan Breen <ryan@alkemi.ai>
2021-01-03 14:59:42 -0500

    build: fork compound safe app form gnosis

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,25 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
+import GlobalStyles from "./global";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import getTheme from "./customTheme";
 
-// theme
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-
-import "react-perfect-scrollbar/dist/css/styles.css";
-import "assets/scss/style.scss";
-import theme from "themes/theme";
-
-// SafeProvider
-import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
-
-// hot reload
-import { hot } from "react-hot-loader/root";
-
-import SafeApp from './SafeApp';
-
-export class App extends Component {
-  render() {
-      return (
-        <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <SafeProvider
-              loader={
-                <>
-                  Waiting for Safe...
-                </>
-              }
-            >
-              <SafeApp />
-            </SafeProvider>
-        </MuiThemeProvider>
-      );
-  }
-}
-
-const AppWithHotReload = hot(App);
+const theme = getTheme();
 
 ReactDOM.render(
-  <AppWithHotReload />,
-  document.getElementById("root") || document.createElement("div")
+  <>
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </>,
+  document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
