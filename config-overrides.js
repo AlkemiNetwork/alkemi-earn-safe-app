bd2a98088bc833705dcedd8042b2d6c138f1aaf0
Ryan Breen <ryan@alkemi.ai>
2021-01-08 17:23:49 -0500

    fix: allow origin header authorization

--- config-overrides.js
+++ config-overrides.js
@@ -1,24 +1,31 @@
 module.exports = {
-  webpack: function (config) {
-    return config;
-  },
-  jest: function (config) {
-    return config;
-  },
-  devServer: function (configFunction) {
-    return function (proxy, allowedHost) {
-      const config = configFunction(proxy, allowedHost);
+    webpack: function (config) {
+        config.headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers":
+                "X-Requested-With, content-type, Authorization",
+        };
+        return config;
+    },
+    jest: function (config) {
+        return config;
+    },
+    devServer: function (configFunction) {
+        return function (proxy, allowedHost) {
+            const config = configFunction(proxy, allowedHost);
 
-      config.headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      };
+            config.headers = {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET",
+                "Access-Control-Allow-Headers":
+                    "X-Requested-With, content-type, Authorization",
+            };
 
-      return config;
-    };
-  },
-  paths: function (paths) {
-    return paths;
-  },
+            return config;
+        };
+    },
+    paths: function (paths) {
+        return paths;
+    },
 };
